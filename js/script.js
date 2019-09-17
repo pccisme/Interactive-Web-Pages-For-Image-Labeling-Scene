9a1f218067937888c5c0ed1138be17de6e20ba80
Pe-Chih Chao <pccisme@gmail.com>
2019-09-17 16:26:16 -0500

    update js css html

--- js/script.js
+++ js/script.js
@@ -1,23 +1,28 @@
 form.onsubmit = function(e) {
 	e.preventDefault();
-	
 
 	var checks = form.elements["checkBox"];
 	var num_checks = checks.length;
 	var count = 0;
+
 	for (var pos=0; pos < num_checks; pos++) {
 		var isChecked = checks[pos].checked == true;
 		if (isChecked) {
 			checkRes = checks[pos].value;
-			console.log(checkRes);
+			console.log(checkRes);			
 		}
 		else {
 			count ++;
 		}
 	}
+
 	if (count === num_checks) {
 		console.log("User submitted the HIT but did not select any image.");
 		alert("You DID NOT select any image!");
-		
 	}
+	else {
+		var comment = form.elements["comment"];
+			console.log("Comment: " + comment.value);
+	}
+
 }
